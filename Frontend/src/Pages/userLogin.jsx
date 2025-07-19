c6d29af4137043f961efec261a26a58861861965
Bhush_Ahire <159047235+Bhush8766@users.noreply.github.com>
2025-07-19 20:34:58 +0530

    Update userLogin.jsx

--- Frontend/src/Pages/userLogin.jsx
+++ Frontend/src/Pages/userLogin.jsx
@@ -22,7 +22,7 @@ const userLogin = () => {
     };
 
     const responce = await axios.post(
-      `${import.meta.env.VITE_BASE_URL}/users/login`,
+      `${import.meta.env.VITE_BASE_URL}https://car-selling-backend-yhh1.onrender.com`,
       userData
     );
 
