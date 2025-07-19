72d9b7a78dd6417d542f88b0ee5fa6b9ca3437e3
Bhush_Ahire <159047235+Bhush8766@users.noreply.github.com>
2025-07-19 21:28:26 +0530

    Update userRegister.jsx

--- Frontend/src/Pages/userRegister.jsx
+++ Frontend/src/Pages/userRegister.jsx
@@ -26,7 +26,7 @@ const userRegister = () => {
     };
 
     const responce = await axios.post(
-      `${import.meta.env.VITE_BASE_URL}https://car-selling-backend-yhh1.onrender.com`,
+      `${import.meta.env.VITE_BASE_URL}/users/register`,
       newUser
     );
 
